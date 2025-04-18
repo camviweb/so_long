b4214fa0372f9b114d8d2944613f326a3ccfa15c
camviweb <camvicreations@gmail.com>
2025-04-18 23:18:46 +0200

    comments

--- color.c
+++ color.c
@@ -1,8 +1,8 @@
 #include "so_long.h"
 
-int	rgb(int r, int g, int b)
+int	create_trgb(int t, int r, int g, int b)
 {
-	return ((r << 16) | (g << 8) | b);
+	return (t << 24 | r << 16 | g << 8 | b);
 }
 
 void	draw_horizontal_gradient(t_data *img, int x, int y, int width,
@@ -16,7 +16,7 @@ void	draw_horizontal_gradient(t_data *img, int x, int y, int width,
 	{
 		red = 255 - (i * 255 / width);
 		blue = (i * 255 / width);
-		color = rgb(red, 0, blue);
+		color = create_trgb(0, red, 0, blue);
 		for (int j = 0; j < height; j++)
 			my_mlx_pixel_put(img, x + i, y + j, color);
 	}
@@ -36,8 +36,28 @@ void	draw_rainbow(t_data *img, int x, int y, int width)
 		r = (int)(9 * (1 - t) * t * t * t * 255);
 		g = (int)(15 * (1 - t) * (1 - t) * t * t * 255);
 		b = (int)(8.5 * (1 - t) * (1 - t) * (1 - t) * t * 255);
-		color = rgb(r, g, b);
+		color = create_trgb(t, r, g, b);
 		for (int j = 0; j < 50; j++)
 			my_mlx_pixel_put(img, x + i, y + j, color);
 	}
+}
+
+int	get_t(int trgb)
+{
+	return ((trgb >> 24) & 0xFF);
+}
+
+int	get_r(int trgb)
+{
+	return ((trgb >> 16) & 0xFF);
+}
+
+int	get_g(int trgb)
+{
+	return ((trgb >> 8) & 0xFF);
+}
+
+int	get_b(int trgb)
+{
+	return (trgb & 0xFF);
 }
--- so_long.h
+++ so_long.h
@@ -8,8 +8,8 @@ typedef struct s_data
 	void	*img;
 	char	*addr;
 	int		bits_per_pixel;
-	int		line_length;
-	int		endian;
+	int line_length; // ?
+	int endian;      // ?
 }			t_data;
 
 void		my_mlx_pixel_put(t_data *data, int x, int y, int color);
@@ -22,8 +22,8 @@ void		draw_triangle(t_data *img, int x, int y, int size, int color);
 void		draw_hexagon(t_data *img, int x_center, int y_center, int size,
 				int color);
 
-// color 
-int			rgb(int r, int g, int b);
+// color
+int			create_trgb(int t, int r, int g, int b);
 void		draw_horizontal_gradient(t_data *img, int x, int y, int width,
 				int height);
 void		draw_rainbow(t_data *img, int x, int y, int width);
